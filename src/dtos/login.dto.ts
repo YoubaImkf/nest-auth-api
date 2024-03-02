41ba5317b99ecfbbc5962bc57227c38fd68c06b9
Youba.I <mevshypocrisie@yahoo.com>
2024-03-02 16:33:12 +0100

    🎨Improve format of code

--- src/dtos/login.dto.ts
+++ src/dtos/login.dto.ts
@@ -1,8 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsString,
-} from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty()
