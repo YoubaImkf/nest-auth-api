81d75f5ca4da249cf08124cc4d30a28f734bfeb1
Youba.I <mevshypocrisie@yahoo.com>
2024-03-07 19:20:07 +0100

    💩Update constants auth file

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,2 +1,5 @@
-export const tokenChars =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+export const tokenConstants = {
+  Chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~',
+  prefix: 'oat_',
+  salt: '<H(hJ@6hsk)%AùOsLén&.f765gidà~lnDAZ1nb/Qwn<ng#{°Bdd98aDDdzakflkjh^é&hb},;,lz=]+KH3dDAZJN?$$$_BG',
+};
